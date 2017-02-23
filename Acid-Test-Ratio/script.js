13227d5bde09f1d3c6bf3953a818585c8c8c4616
mikestratton <mike@mikestratton.net>
2017-02-23 01:02:36 -0500

    Acid Test Calculator aka Quick Ratio

---
+++ Acid-Test-Ratio/script.js
@@ -0,0 +1,14 @@
+$('#assets').keyup(calculateRatio);
+$('#inventory').keyup(calculateRatio);
+$('#liability').keyup(calculateRatio);
+
+function calculateRatio() {
+  
+  var a = parseInt($('#assets').val());
+  var b = parseInt($('#inventory').val());
+  var c = parseInt($('#liability').val());
+  var d = (a / c) + ":";
+  var e = (b / c) + ":";
+  var f = 1;
+   $('p#ratio').text(d+e+f);
+}
