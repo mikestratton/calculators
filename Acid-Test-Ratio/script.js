dfdcf7b16a92a60bdee0ea822b71d3789f895c41
mikestratton <mike@mikestratton.net>
2017-02-23 04:09:51 -0500

    css styling

--- Acid-Test-Ratio/script.js
+++ Acid-Test-Ratio/script.js
@@ -6,9 +6,12 @@ function calculateRatio() {
   
   var a = parseInt($('#assets').val());
   var b = parseInt($('#inventory').val());
-  var c = parseInt($('#liability').val());
+  var c = parseInt($('#liability').val());  
+
   var d = (a / c) + ":";
   var e = (b / c) + ":";
   var f = 1;
-   $('p#ratio').text(d+e+f);
+
+  $('p#ratio').text(d+e+f);
+
 }
