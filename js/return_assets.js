da8af7d0e22685e162c14f7313735b28b0802b6d
mikestratton <mike@mikestratton.net>
2017-07-25 00:40:27 -0400

    return on assets nan error removed

---
+++ js/return_assets.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    returnassets();
+
+    $("#num1, #num2").on("keydown keyup", function() {
+        returnassets();
+    });
+
+});
+
+function returnassets() {
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var result = parseInt(num1) / parseInt(num2);
+    var rfixed = result.toFixed(2);
+    var rtext = ':1';
+
+    if (!isNaN(rfixed)) {
+        document.getElementById('returnassets').value = rfixed + rtext;
+    }
+}
