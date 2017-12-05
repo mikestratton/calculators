4d77ad6a035cfcaa2e66ea324c79c20f809838de
mikestratton <mike@mikestratton.net>
2017-12-05 15:30:37 -0500

    displaying percentage

--- js/gross_profit_margin.js
+++ js/gross_profit_margin.js
@@ -12,9 +12,10 @@ function grossprofit() {
     var num2 = document.getElementById('num2').value;
     var result = (parseInt(num1) - parseInt(num2)) / (parseInt(num1));
     var rfixed = result.toFixed(2);
-    var rtext = ':1';
+    var percent = rfixed * 100;
+    var rtext = '%';
 
     if (!isNaN(rfixed)) {
-        document.getElementById('grossprofit').value = rfixed + rtext;
+        document.getElementById('grossprofit').value = percent + rtext;
     }
 }
